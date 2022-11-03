a9ae25f0a3f7ee786880849a00c64854fbe934c2
SDen4 <asw325@yandex.ru>
2022-11-03 21:35:17 +0500

    feat: add current block size

--- src/modules/btcBlockInfo.js
+++ src/modules/btcBlockInfo.js
@@ -8,11 +8,13 @@ const btcBlockInfo = async (bot, chatId) => {
   if (allData?.length) {
     await bot.sendMessage(
       chatId,
-      `Last block Id: ${id}\nCurrent block median fee: ${Math.ceil(
+      `Last block Id: ${id}\n------------------------------\nCurrent block median fee: ${Math.ceil(
         allData[0].medianFee,
       ).toFixed()} sat/vB\nCurrnet block total fees: ${(
         Math.ceil(allData[0].totalFees) / 100000000
-      ).toFixed(3)} BTC`,
+      ).toFixed(3)} BTC\nCurrent block size: ${(
+        allData[0].blockSize / 1000000
+      ).toFixed(2)} MB`,
     );
   } else {
     await bot.sendMessage(chatId, 'No data');
