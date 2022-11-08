109e4c174df479bf41766bbb8f30906f4c493bca
SDen4 <asw325@yandex.ru>
2022-11-08 20:56:42 +0500

    feat: excluding form stat

--- src/modules/stat.js
+++ src/modules/stat.js
@@ -2,6 +2,9 @@ const { statChatId } = require('../../token');
 
 const stat = async (bot, msg) => {
   const command = msg?.data || msg?.text;
+
+  if (msg.from.id === statChatId) return;
+
   await bot.sendMessage(
     statChatId,
     `User ${msg.from.id} (${msg?.from?.first_name} ${msg?.from?.last_name}, ${msg?.from?.language_code}) requests ${command}`,
