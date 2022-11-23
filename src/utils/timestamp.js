36e4b2ad1f99f40292852af80f88477f51dee012
SDen4 <asw325@yandex.ru>
2022-11-23 15:41:25 +0500

    refactor: timestamp func

--- src/utils/timestamp.js
+++ src/utils/timestamp.js
@@ -5,16 +5,19 @@
  * @return {*} string 'dd.mm.yyyy hh.mm'
  */
 const timestamp = (timestamp) => {
+  const formatValue = (val) => (String(val).length === 1 ? `0${val}` : val);
+
   const rightTimeStamp =
     String(timestamp).length <= 10 ? timestamp * 1000 : timestamp;
 
   const date = new Date(rightTimeStamp);
-  const month = date.getMonth() + 1;
-  const correctMonth = String(month).length === 1 ? `0${month}` : month;
-  const hour = date.getHours();
-  const min = date.getMinutes();
 
-  return `${date.getDate()}.${correctMonth}.${date.getFullYear()} ${hour}:${min}`;
+  const day = formatValue(date.getDate());
+  const month = formatValue(date.getMonth() + 1);
+  const hour = formatValue(date.getHours());
+  const min = formatValue(date.getMinutes());
+
+  return `${day}.${month}.${date.getFullYear()} ${hour}:${min}`;
 };
 
 module.exports = { timestamp };
