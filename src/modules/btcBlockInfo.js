9e71365d62b08268dbfd637f86c4241e24ff70e4
SDen4 <asw325@yandex.ru>
2023-03-19 22:28:03 +0500

    fix bug of catch on server

--- src/modules/btcBlockInfo.js
+++ src/modules/btcBlockInfo.js
@@ -11,16 +11,17 @@ const btcBlockInfo = async (bot, chatId) => {
   const allData = await mpCurBlockRequest();
   const id = await mpIdRequest();
   const hash = await mpHashRequest();
-
   const lastBlock = await mpLastBlockRequest(hash);
-  const date = await timestamp(lastBlock.timestamp, true);
 
   if (allData?.length) {
     await bot.sendMessage(
       chatId,
       `Last block:\n - id: ${id}\n - transactions: ${
         lastBlock?.tx_count || '?'
-      }\n - time: ${date}\n-------------------------------\nCurrent block:\n - median fee: ${Math.ceil(
+      }\n - time: ${timestamp(
+        lastBlock?.timestamp,
+        true,
+      )}\n-------------------------------\nCurrent block:\n - median fee: ${Math.ceil(
         allData[0].medianFee,
       ).toFixed()} sat/vB\n - total fees: ${(
         Math.ceil(allData[0].totalFees) / 100000000
