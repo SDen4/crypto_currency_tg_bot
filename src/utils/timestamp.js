668f0615e0deec6f37ef95f2edd79fb0f4f099ce
SDen4 <asw325@yandex.ru>
2023-03-20 21:28:24 +0500

    debug server time

--- src/utils/timestamp.js
+++ src/utils/timestamp.js
@@ -7,12 +7,10 @@
 const timestamp = (timestamp, isTime = false) => {
   const formatValue = (val) => (String(val).length === 1 ? `0${val}` : val);
 
-  const offSet = 300000;
-
-  console.log(offSet);
+  const offSet = 300000; // server time
 
   const rightTimeStamp =
-    (String(timestamp).length <= 10 ? timestamp * 1000 : timestamp) - offSet;
+    (String(timestamp).length <= 10 ? timestamp * 1000 : timestamp) + offSet;
 
   const date = new Date(rightTimeStamp);
 
@@ -22,7 +20,7 @@ const timestamp = (timestamp, isTime = false) => {
   if (isTime) {
     const sec = formatValue(date.getUTCSeconds());
 
-    const deltaMs = new Date().getTime() - offSet - rightTimeStamp;
+    const deltaMs = new Date().getTime() + offSet - rightTimeStamp;
     const deltaStr = `${Math.floor(deltaMs / 60000)} min ago`;
 
     return `${hour}:${min}:${sec} (${deltaStr})`;
