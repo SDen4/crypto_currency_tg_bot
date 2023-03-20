b2ec9bacda3c8b30f5be412e0323b592b23f4c31
SDen4 <asw325@yandex.ru>
2023-03-20 21:20:02 +0500

    feat: update timestamp func

--- src/utils/timestamp.js
+++ src/utils/timestamp.js
@@ -7,20 +7,29 @@
 const timestamp = (timestamp, isTime = false) => {
   const formatValue = (val) => (String(val).length === 1 ? `0${val}` : val);
 
+  const offSet = new Date().getTimezoneOffset() * 60000;
+
   const rightTimeStamp =
-    String(timestamp).length <= 10 ? timestamp * 1000 : timestamp;
+    (String(timestamp).length <= 10 ? timestamp * 1000 : timestamp) - offSet;
 
   const date = new Date(rightTimeStamp);
 
-  const day = formatValue(date.getDate());
-  const month = formatValue(date.getMonth() + 1);
-  const hour = formatValue(date.getHours());
-  const min = formatValue(date.getMinutes());
-  const sec = formatValue(date.getSeconds());
+  const hour = formatValue(date.getUTCHours());
+  const min = formatValue(date.getUTCMinutes());
 
-  if (isTime) return `${hour}:${min}:${sec}`;
+  if (isTime) {
+    const sec = formatValue(date.getUTCSeconds());
 
-  return `${day}.${month}.${date.getFullYear()} ${hour}:${min}`;
+    const deltaMs = new Date().getTime() - offSet - rightTimeStamp;
+    const deltaStr = `${Math.floor(deltaMs / 60000)} min ago`;
+
+    return `${hour}:${min}:${sec} (${deltaStr})`;
+  }
+
+  const day = formatValue(date.getUTCDate());
+  const month = formatValue(date.getUTCMonth() + 1);
+
+  return `${day}.${month}.${date.getUTCFullYear()} ${hour}:${min}`;
 };
 
 module.exports = { timestamp };
