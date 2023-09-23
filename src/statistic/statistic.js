4423ea7e350a2e2b6b98faa30feab82de281ab01
SDen4 <asw325@yandex.ru>
2023-09-23 12:29:04 +0500

    update statistics logic

--- src/statistic/statistic.js
+++ src/statistic/statistic.js
@@ -6,15 +6,11 @@ const saveStat = async (visit) => {
   const id = visit?.from?.id || visit?.chat?.id;
   if (id === statChatId) return;
 
-  await axios.post(statUrl, visit).then((response) => {
-    return response;
-  });
+  await axios.post(statUrl, visit).then((response) => response);
 };
 
 const getStat = async () => {
-  const res = await axios.get(statUrl).then((response) => {
-    return response;
-  });
+  const res = await axios.get(statUrl).then((response) => response);
 
   return res;
 };
