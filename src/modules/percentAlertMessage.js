85acf3d47d541ece886930388229268207d398a8
SDen4 <asw325@yandex.ru>
2024-03-05 20:01:07 +0500

    update alert settings

--- src/modules/percentAlertMessage.js
+++ src/modules/percentAlertMessage.js
@@ -2,8 +2,8 @@ const { statChatId } = require('../../token');
 const { bfHttpInfoRequest } = require('../api/bfHttpInfoRequest');
 
 const percentAlertMessage = (bot) => {
-  const timeInMinutes = 30;
-  const timer = setInterval(async() => await bfHttpInfoRequest(bot, statChatId, '/btcusd', 3), timeInMinutes * 60000);
+  const timeInMinutes = 60;
+  const timer = setInterval(async() => await bfHttpInfoRequest(bot, statChatId, '/btcusd', 5), timeInMinutes * 60000);
 };
 
 module.exports = { percentAlertMessage };
