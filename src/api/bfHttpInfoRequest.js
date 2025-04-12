960c9faad9a08ad47dca9bdc38c187837897c83e
SDen4 <asw325@yandex.ru>
2025-04-12 15:41:32 +0500

    feat: update percent alert message logic

--- src/api/bfHttpInfoRequest.js
+++ src/api/bfHttpInfoRequest.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { formatNumber } from '../utils/formatNumber.js';
 import { bfUrl, statChatId } from '../../token.js';
 
+let prevValue = 0;
+
 export const bfHttpInfoRequest = (bot, chatIdArr, textInner, alertPersent) => {
   const pathParams = 'ticker';
   const text = textInner[0] === '/' ? textInner : `/${textInner}`;
@@ -13,32 +15,34 @@ export const bfHttpInfoRequest = (bot, chatIdArr, textInner, alertPersent) => {
     (response) => {
       const data = response.data;
 
-      let title =
-        String(text).length === 7
-          ? `${text.toLocaleUpperCase().slice(1, 4)}/${text
-              .toLocaleUpperCase()
-              .slice(4)}`
-          : `${String(text).slice(1).toLocaleUpperCase().replace(':', '/')}`;
-      // exceptions
-      if (text === '/ustusd') title = 'USDt/USD';
-      if (text === '/udcusd') title = 'USDC/USD';
-      if (text === '/iotusd') title = 'IOTA/USD';
-      if (text === '/algusd') title = 'ALGO/USD';
-      if (text === '/hixusd') title = 'HI/USD';
-      if (text === '/dshusd') title = 'DASH/USD';
+      const number = Math.abs(data[5] * 100); // % difference
 
-      // ============================== answer text, do not format ============================== //
-      const number = Math.abs(data[5] * 100);
-      const emojiL = data[5] > 0 ? '🟢' : '❗️🔻';
-      const emojiR = data[5] > 0 ? '🟢' : '🔻❗️';
-      const answer = `
+      console.log('number: ', number);
+
+      // HERE add conditon!!!
+      if (number >= alertPersent && number > (prevValue * alertPersent) / 100) {
+        prevValue = number;
+
+        let title =
+          String(text).length === 7
+            ? `${text.toLocaleUpperCase().slice(1, 4)}/${text
+                .toLocaleUpperCase()
+                .slice(4)}`
+            : `${String(text).slice(1).toLocaleUpperCase().replace(':', '/')}`;
+        // exceptions
+        if (text === '/ustusd') title = 'USDt/USD';
+        if (text === '/udcusd') title = 'USDC/USD';
+        if (text === '/iotusd') title = 'IOTA/USD';
+        if (text === '/algusd') title = 'ALGO/USD';
+        if (text === '/hixusd') title = 'HI/USD';
+        if (text === '/dshusd') title = 'DASH/USD';
+
+        const emoji = data[5] > 0 ? '🟢' : '🔻';
+        const answer = `
 ${title}: ${formatNumber(data[0])}
 -------------------
-${emojiL} 24h: ${formatNumber(number, 2, '%')} ${emojiR}
-`;
-      // ============================== answer text, do not format ============================== //
+${emoji} 24h: ${formatNumber(number, 2, '%')} ${emoji}`;
 
-      if (number >= alertPersent) {
         for (let i = 0; i < chatIdArr.length; i++) {
           bot.sendMessage(chatIdArr[i], answer);
         }
