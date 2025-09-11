939ad5d3c09dc459c1b446b7a3c9819569ac4ba8
SDen4 <asw325@yandex.ru>
2025-09-11 19:29:09 +0500

    fix bug of banned users

--- src/api/checkBannedUsers.js
+++ src/api/checkBannedUsers.js
@@ -7,7 +7,7 @@ export const checkBannedUsers = async (bot, chatId) => {
   if (chatId === statChatId) return false;
 
   const data = await axios
-    .get(`${statUrl}bannedIds!.json`)
+    .get(`${statUrl}bannedIds.json`)
     .then((response) => response)
     .catch((error) =>
       sendErrorMessage(`Error in checkBannedUsers: ${error}`, bot, statChatId),
