31bb7eb534000c700938c504e8ff04395a41b677
SDen4 <asw325@yandex.ru>
2025-09-24 20:02:47 +0500

    refactor: update user limits message

--- src/modules/limits.js
+++ src/modules/limits.js
@@ -7,6 +7,9 @@ import { periodOfLimitRequests } from '../constants/index.js';
 export const limits = async (bot, chatId, msg) => {
   try {
     const user = await getFiatUser(bot, chatId);
+    const restTime =
+      periodOfLimitRequests - (new Date().getTime() - user.lastVisit);
+    const isTimeExpired = restTime <= 0;
 
     await bot.sendMessage(
       chatId,
@@ -14,13 +17,13 @@ export const limits = async (bot, chatId, msg) => {
         msg?.from?.first_name
       }, your limits:\n------------------\n1. fiat rates: ${
         user
-          ? `${user.limit}/day (last 24h used: ${user.count})`
+          ? `${user.limit}/day (last 24h used: ${
+              isTimeExpired ? 0 : user.count
+            })`
           : `${fiatRequestsLimit}/day`
       }\n${
-        user && user.count === user.limit
-          ? `available in ${msToStr(
-              periodOfLimitRequests - (new Date().getTime() - user.lastVisit),
-            )}`
+        user && user.count === user.limit && !isTimeExpired
+          ? `available in ${msToStr(restTime)}`
           : ''
       }\n2. crypto rates: unlimited`,
     );
