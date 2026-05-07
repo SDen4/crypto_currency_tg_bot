ea941411c751be88d685a0e9cc4c41c5e8c7bf00
SDen4 <asw325@yandex.ru>
2026-05-07 21:37:08 +0500

    fix: fix bugo in fiat history

--- src/utils/fiatHistoryApi.js
+++ src/utils/fiatHistoryApi.js
@@ -1,5 +1,5 @@
-const fiatHistoryApiUrl = process.env.FIAT_HISTORY_API_KEY;
-const fiatHistoryApiKey = process.env.FIAT_HISTORY_API_URL;
+const fiatHistoryApiUrl = process.env.FIAT_HISTORY_API_URL;
+const fiatHistoryApiKey = process.env.FIAT_HISTORY_API_KEY;
 
 export const fiatHistoryApi = (from, to) =>
   `${fiatHistoryApiUrl}${from}&to_symbol=${to}&apikey=${fiatHistoryApiKey}&outputsize=full`;
